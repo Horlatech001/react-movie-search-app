1b5ef71a4da4e563e8032baf4d4d8327881bc456
Horlatech001 <oladimejigobir@gmail.com>
2023-01-10 02:03:06 +0100

    update

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -3,7 +3,7 @@ const MovieList = ({ movies, favoriteComponent, handleFavouritesClick }) => {
   const FavoriteComponent = favoriteComponent;
   return (
     <>
-      {movies.map((movie, index) => (
+      {movies?.map((movie, index) => (
         <div className="img-container" key={movie.imdbID}>
           <img src={movie.Poster} alt="img-result" />
           <div className="overlay" onClick={() => handleFavouritesClick(movie)}>
